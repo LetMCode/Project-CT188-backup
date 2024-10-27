2dd79c0b38dbe411888bac7ab3aae4b807d3f836
LetMCode <hoab2203718@student.ctu.edu.vn>
2024-10-27 16:25:40 +0700

    update

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -62,7 +62,7 @@ function toastMessage() {
     for (let btn of btnOther) {
         btn.addEventListener('click', () => {
             toastMsg.appendChild(contentToastMsg);
-            toastMsg.style.display = 'flex'
+            toastMsg.style.display = 'block'
             contentToastMsg.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
             clearTimeout(autoRemoveId)
         });
